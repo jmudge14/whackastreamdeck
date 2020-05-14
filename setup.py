024a23df286691aeb0f9a6392f49aa3d2a0ae6f1
Jack Mudge <jmudge14@gmail.com>
2020-05-14 08:36:21 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+        name="whackastreamdeck",
+        version="0.0.1",
+        author="Jack Mudge",
+        author_email="jack@mudge.dev",
+        packages=["whackastreamdeck"],
+        scripts=[],
+        url="https://github.com/jmudge14/whackastreamdeck",
+        license="LICENSE",
+        description="A Whack-A-Mole game for the Elgato Stream Deck",
+        long_description=open("README.md","r").read(),
+        install_requires=[ 
+            "streamdeck",
+            "pillow"
+        ],
+)
+        
+
